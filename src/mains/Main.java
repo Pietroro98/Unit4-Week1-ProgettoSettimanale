f40c5d228215e4aca991742a31f3c6b9dfc52dbd
Pietroro98 <pietroro98@gmail.com>
2024-09-27 11:55:38 +0200

    inserirsco cinque elementi multimediali (immagine, audio o video) e li memorizzoi in un array di oggetti multimediali con lunghezza di 5. Do imput da tastiera per eseguire le azioni  usando uno switch per la creazione e memorizzazione degli ogetti

--- src/mains/Main.java
+++ src/mains/Main.java
@@ -1,6 +1,7 @@
 package mains;
 
 import model.ElementoMultimediale;
+import model.Video;
 
 import java.util.Scanner;
 
@@ -9,7 +10,40 @@ public class Main {
     private static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Inserisci l'elemento (1-Immagine, 2-Audio, 3-Video) da visualizzare: ");
+            int opzione = sc.nextInt();
+            sc.nextLine();
+            System.out.println("Inserisci il un titolo: ");
+            String titolo = sc.nextLine();
+            switch (opzione) {
+                case 1:
+                    System.out.println("inserisci la luminosita desiderata: ");
+                    int luminosita = sc.nextInt();
+                    elementi[i] = new model.Immagine(titolo, luminosita);
+                    break;
+                case 2:
+                    System.out.println("inserisci la durata desiderata: ");
+                    int durataAudio = sc.nextInt();
+                    System.out.println("inserisci il volume desiderato: ");
+                    int volumeAudio = sc.nextInt();
+                    elementi[i] = new model.RegistrazioneAudio(titolo, durataAudio, volumeAudio );
+                    break;
+                case 3:
+                    System.out.println("inserisci la durata desiderata: ");
+                    int durataVideo = sc.nextInt();
+                    System.out.println("inserisci il volume desiderato: ");
+                    int VolumeVideo = sc.nextInt();
+                    System.out.println("inserisci la luminosita desiderata: ");
+                    int luminositaVideo = sc.nextInt();
+                    elementi[i] = new Video(titolo, durataVideo, VolumeVideo, luminositaVideo);
+                    break;
+                    default:
+                    System.out.println("!! WARNING !! - L'opzione che hai inserito non é valida");
+                    i--;
+                    break;
+            }
+        }
 
     }
 }
